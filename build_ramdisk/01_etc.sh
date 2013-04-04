59589b3975a28603c790f961a39b129da4a25fa2
Tomonori Kimura <kimura@plathome.co.jp>
2013-04-04 14:12:58 +0900

    Revise init order.

--- build_ramdisk/01_etc.sh
+++ build_ramdisk/01_etc.sh
@@ -8,7 +8,19 @@
 (cd ${ETCDIR_ADD};tar --exclude=CVS -cvf - .)| tar xvf - -C ${DISTDIR}/etc/
 
 
-for sh in openblocks-setup pshd runled;do
+for sh in openblocks-setup pshd runled ;do
 	chmod 755 ${DISTDIR}/etc/init.d/$sh
-	chroot ${DISTDIR} /usr/sbin/update-rc.d $sh defaults
+	chroot ${DISTDIR} /usr/sbin/update-rc.d -f $sh remove
+	case $sh in
+	openblocks-setup)
+		chroot ${DISTDIR} /usr/sbin/update-rc.d $sh start 1 S . stop 1 0 6 .
+		;;
+	*)
+		chroot ${DISTDIR} /usr/sbin/update-rc.d $sh defaults
+		;;
+	esac
 done
+
+touch ${DISTDIR}/etc/init.d/.legacy-bootordering
+
+printf "0.0 0 0.0\n0\nUTC\n" > ${DISTDIR}/etc/adjtime
