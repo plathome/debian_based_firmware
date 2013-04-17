1d0ac486302aec3fbd232b8092b9f8bdfd167fd0
Tomonori Kimura <kimura@plathome.co.jp>
2013-04-17 19:28:07 +0900

    Add uboot-envtools package to ramdisk image.

--- build_ramdisk/_00_debootstrap.sh
+++ build_ramdisk/_00_debootstrap.sh
@@ -7,7 +7,7 @@ mkdir -p ${DISTDIR}
 
 debootstrap --arch=${ARCH} \
 	--exclude=quik,mac-fdisk,amiga-fdisk,hfsutils,yaboot,powerpc-utils,powerpc-ibm-utils,nano \
-	--include=gnu-fdisk,udev,openssh-server,lzma,strace \
+	--include=gnu-fdisk,udev,openssh-server,lzma,strace,uboot-envtools \
 	${DIST} ${DISTDIR}
 
 rm -f  ${DISTDIR}/etc/udev/rules.d/70-persistent-net.rules
