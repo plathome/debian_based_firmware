1c03906fd6e1aa77e20f285831dbed52aefc9f29
Tomonori Kimura <kimura@plathome.co.jp>
2013-05-22 15:24:01 +0900

    Support cross install

--- build_ramdisk/11_install_commands.sh
+++ build_ramdisk/11_install_commands.sh
@@ -11,5 +11,6 @@ chmod 555 ${DISTDIR}/usr/sbin/usbreset
 (cd ${FILESDIR}; ./build_obstools.sh $TARGET) || exit 1
 
 for cmd in flashcfg-debian runled pshd; do
-	(cd ${FILESDIR}; install -c -s -o root -g root -m 555 $cmd ${DISTDIR}/usr/sbin/$cmd)
+	(cd ${FILESDIR}; install -c -o root -g root -m 555 $cmd ${DISTDIR}/usr/sbin/$cmd)
+	$STRIP ${DISTDIR}/usr/sbin/$cmd
 done
