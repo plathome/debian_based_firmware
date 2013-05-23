c0a3efa7e16770613204382901877bf7f46e35cf
Tomonori Kimura <kimura@plathome.co.jp>
2013-05-23 17:40:59 +0900

    Add qemu-user-static

--- build_ramdisk/build_crossenv.sh
+++ build_ramdisk/build_crossenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . `dirname $0`/config.sh
 
-packages="build-essential uboot-mkimage libncurses5-dev libc6-armel-cross libc6-dev-armel-cross binutils-arm-linux-gnueabi gcc-4.7-arm-linux-gnueabi zlib1g-dev-armel-cross vim"
+packages="build-essential uboot-mkimage libncurses5-dev libc6-armel-cross libc6-dev-armel-cross binutils-arm-linux-gnueabi gcc-4.7-arm-linux-gnueabi zlib1g-dev-armel-cross vim qemu-user-static"
 
 cat <<_EOF >/etc/apt/sources.list.d/emdebian.list 
 deb http://www.emdebian.org/debian wheezy main
