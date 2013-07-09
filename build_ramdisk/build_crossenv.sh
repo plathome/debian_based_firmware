84fc11ffdb5f6898dadca41857433f17a7414e1c
Jiro Shimura <j-shimura@plathome.co.jp>
2013-07-09 16:17:37 +0900

    for obs600 wheezy

--- build_ramdisk/build_crossenv.sh
+++ build_ramdisk/build_crossenv.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 . `dirname $0`/config.sh
 
-packages="build-essential uboot-mkimage libncurses5-dev debootstrap libc6-armel-cross libc6-dev-armel-cross binutils-arm-linux-gnueabi zlib1g-dev-armel-cross vim qemu-user-static"
+packages="build-essential uboot-mkimage libncurses5-dev debootstrap libc6-${ARCH}-cross libc6-dev-${ARCH}-cross binutils-${KERN_ARCH}-linux-gnu${ABI} zlib1g-dev-${ARCH}-cross vim qemu-user-static"
 
 host_debian_version=`cat /etc/debian_version`
 
 case $host_debian_version in
 	6.*)
 		code_name=squeeze
-		gcc_version=4.4
-		packages="$packages gcc-${gcc_version}-arm-linux-gnueabi xz-lzma"
+		if [ "$TARGET" == "obs600" ];then
+			gcc_version=4.3
+		else
+			gcc_version=4.4
+		fi
+		packages="$packages gcc-${gcc_version}-${KERN_ARCH}-linux-gnu${ABI} xz-lzma"
 	;;
 	7.*)
 		code_name=wheezy
 		gcc_version=4.7
-		packages="$packages gcc-${gcc_version}-arm-linux-gnueabi"
+		packages="$packages gcc-${gcc_version}-${KERN_ARCH}-linux-gnu${ABI}"
 	;;
 esac
 
@@ -25,7 +29,17 @@ _EOF
 apt-get update 
 apt-get install $packages
 
-update-alternatives --install /usr/bin/arm-linux-gnueabi-gcc arm-linux-gnueabi-gcc /usr/bin/arm-linux-gnueabi-gcc-${gcc_version} 255
-update-alternatives --install /usr/bin/arm-linux-gnueabi-cpp arm-linux-gnueabi-cpp /usr/bin/arm-linux-gnueabi-cpp-${gcc_version} 255
+update-alternatives --install /usr/bin/${KERN_ARCH}-linux-gnu${ABI}-gcc ${KERN_ARCH}-linux-gnu${ABI}-gcc /usr/bin/${KERN_ARCH}-linux-gnu${ABI}-gcc-${gcc_version} 255
+update-alternatives --install /usr/bin/${KERN_ARCH}-linux-gnu${ABI}-cpp ${KERN_ARCH}-linux-gnu${ABI}-cpp /usr/bin/${KERN_ARCH}-linux-gnu${ABI}-cpp-${gcc_version} 255
 
 update-alternatives --set editor /usr/bin/vim.basic
+
+if [ "$TARGET" == "obs600" ]; then
+	PKGURL="http://ftp.jp.debian.org/debian/pool/main/q/qemu"
+	PKGNAME="qemu-user-static_1.1.2+dfsg-6a_i386.deb"
+	dpkg -r qemu-user-static
+	wget -O /tmp/${PKGNAME} ${PKGURL}/${PKGNAME}
+	dpkg -i /tmp/${PKGNAME}
+	rm -f /tmp/${PKGNAME}
+fi
+
