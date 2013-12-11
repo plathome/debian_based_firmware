9732f0308bbfc6c8c66ecc3d78d0b3aaf04ddfb1
Atsushi Yamagata <yamagata@plathome.co.jp>
2013-12-11 16:16:48 +0900

    Add LICENCE.

--- build_ramdisk/files/flashcfg_obs600.c
+++ build_ramdisk/files/flashcfg_obs600.c
@@ -1,4 +1,32 @@
-/*	$ssdlinux: flashcfg.c,v 1.24 2013/10/15 07:06:38 yamagata Exp $	*/
+/*	$ssdlinux: flashcfg.c,v 1.25 2013/12/11 07:11:52 yamagata Exp $	*/
+/*
+ * Copyright (c) 2008-2013 Plat'Home CO., LTD.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Plat'Home CO., LTD. nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 //#define DEBUG
 
@@ -92,7 +120,7 @@ static char* tarpath;
  *      mtd6: MONITOR_SIZE             "OpenBlockS-OBS600 Uboot"
  */
 
-#define VERSION "$Revision: 1.24 $"
+#define VERSION "$Revision: 1.25 $"
 
 int SECT_SIZE = 0;
 int MONITOR_SIZE = 0;
