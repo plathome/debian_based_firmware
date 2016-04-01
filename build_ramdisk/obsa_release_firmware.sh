cddf9eac9322c221ac3ed33f44b28a1cbaa5b29c
Atsushi Yamagata <yamagata@plathome.co.jp>
2016-04-01 10:30:41 +0900

    Remove conflicts.

--- build_ramdisk/obsa_release_firmware.sh
+++ build_ramdisk/obsa_release_firmware.sh
@@ -81,25 +81,10 @@ fi
 ${COMP} -${COMP_LVL:-3} < ${_RAMDISK_IMG} > ${RELEASEDIR}/${RAMDISK_IMG}.${COMP_EXT}
 
 if [ "$TARGET" == "obs600" ]; then
-<<<<<<< HEAD
-mkimage -n "$(echo ${TARGET}|tr [a-z] [A-Z]) ${VERSION}" \
-	-A ppc -O linux -T multi -C gzip \
-	-d ${RELEASEDIR}/vmlinux.bin.gz:${RELEASEDIR}/${RAMDISK_IMG}.${COMP_EXT}:${LINUX_SRC}/arch/${KERN_ARCH}/boot/${TARGET}.dtb \
-	${RELEASEDIR}/uImage.initrd.${TARGET}
-	case $KERNEL in
-	3.13|4.*)
-		(cd ${WRKDIR}/build_ramdisk/kernel-image; ./mkdeb.sh ${VERSION} ${ARCH} "-${TARGET}" ${RELEASEDIR}/uImage.initrd.${TARGET})
-	;;
-	*)
-		(cd ${WRKDIR}/build_ramdisk/kernel-image; ./mkdeb.sh ${VERSION} ${ARCH} "-${TARGET}" ${RELEASEDIR}/uImage.initrd.${TARGET})
-	;;
-	esac
-=======
 	mkimage -n "$(echo ${TARGET}|tr [a-z] [A-Z]) ${VERSION}" \
 		-A ppc -O linux -T multi -C gzip \
 		-d ${RELEASEDIR}/vmlinux.bin.gz:${RELEASEDIR}/${RAMDISK_IMG}.${COMP_EXT}:${LINUX_SRC}/arch/${KERN_ARCH}/boot/${TARGET}.dtb \
 		${RELEASEDIR}/uImage.initrd.${TARGET}
->>>>>>> b9560dbe1a7d50e0ffe1870a13b8cf00b1606824
 else
 	case $KERNEL in
 	3.13|4.*)
@@ -113,11 +98,6 @@ else
 			-A arm -O linux -T multi -C none -a 0x8000 -e 0x8000 \
 			-d ${RELEASEDIR}/zImage:${RELEASEDIR}/${RAMDISK_IMG}.${COMP_EXT} \
 			${RELEASEDIR}/uImage.initrd.${TARGET}
-<<<<<<< HEAD
-		(cd ${WRKDIR}/build_ramdisk/kernel-image; \
-			./mkdeb.sh ${VERSION} ${ARCH} "-${TARGET}" ${RELEASEDIR}/uImage.initrd.${TARGET})
-=======
->>>>>>> b9560dbe1a7d50e0ffe1870a13b8cf00b1606824
 	;;
 	esac
 fi
