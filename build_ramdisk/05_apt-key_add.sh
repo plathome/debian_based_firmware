d8a4fc39c3aa6c74c1d1b8f9b5f254463dbb29dd
root <j-shimura@plathome.co.jp>
2017-10-10 14:15:05 +0900

    Add Debian9 key file

--- build_ramdisk/05_apt-key_add.sh
+++ build_ramdisk/05_apt-key_add.sh
@@ -27,6 +27,15 @@
 
 . `dirname $0`/config.sh
 
-cp ${FILESDIR}/public_20090828.key ${DISTDIR}/
-chroot ${DISTDIR} /usr/bin/apt-key add /public_20090828.key
-rm -f ${DISTDIR}/public_20090828.key
+case $DIST in
+stretch)
+	keyfile="public_20171002.key"
+	;;
+*)
+	keyfile="public_20090828.key"
+	;;
+esac
+
+cp ${FILESDIR}/${keyfile} ${DISTDIR}/
+chroot ${DISTDIR} /usr/bin/apt-key add /${keyfile}
+rm -f ${DISTDIR}/${keyfile}
