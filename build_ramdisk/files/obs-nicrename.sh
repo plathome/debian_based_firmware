908399c4ba05b5ec703acd21eb11edd9cb921f1e
Tamotsu Kanoh <kanoh@plathome.co.jp>
2023-03-08 13:08:15 +0900

    modified for support net.ifnames=0

--- build_ramdisk/files/obs-nicrename.sh
+++ build_ramdisk/files/obs-nicrename.sh
@@ -28,40 +28,94 @@
 [ -f /etc/default/openblocks ] && . /etc/default/openblocks
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+rename_func() {
+	if [ -x /usr/sbin/ifrename -a -f ${1} ] ; then
+		ifrename -c ${1}
+	fi
+}
+
+cp_func() {
+	if [ ! -s /etc/network/interfaces -a -f ${1} ] ; then
+		cp -p ${1} /etc/network/interfaces
+	fi
+}
+
 case $MODEL in
-obshx1)
+obshx1*)
 	sleep 5
-	if [ -e /sys/class/net/enp2s0 ]; then
-		ifrename -c /etc/network/iftab-obshx1-4port
-		cp -a /etc/network/interfaces-4port /etc/network/interfaces
-	elif [ -e /sys/class/net/enp3s0 ]; then
-		ifrename -c /etc/network/iftab-obshx1-3port
-		cp -a /etc/network/interfaces-3port /etc/network/interfaces
+
+	rules=/etc/udev/rules.d/75-net-description.rules
+
+	net_ifnames=true
+	grep -q 'net.ifnames=0' /proc/cmdline && net_ifnames=false
+
+	if [ -L $rules -o $net_ifnames = false ] ; then
+		if [ -e /sys/class/net/eth3 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-4port_tmp.conf
+			sleep 0.01
+			rename_func /etc/openblocks/rename/${MODEL}-4port.conf
+			cp_func /etc/network/interfaces-4port
+		elif [ -e /sys/class/net/eth2 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-3port_tmp.conf
+			sleep 0.01
+			rename_func /etc/openblocks/rename/${MODEL}-3port.conf
+			cp_func /etc/network/interfaces-3port
+		else
+			rename_func /etc/openblocks/rename/${MODEL}-2port_tmp.conf
+			sleep 0.01
+			rename_func /etc/openblocks/rename/${MODEL}-2port.conf
+			cp_func /etc/network/interfaces-2port
+		fi
 	else
-		ifrename -c /etc/network/iftab-obshx1-2port
-		cp -a /etc/network/interfaces-2port /etc/network/interfaces
+		if [ -e /sys/class/net/enp2s0 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-4port.conf
+			cp_func /etc/network/interfaces-4port
+		elif [ -e /sys/class/net/enp3s0 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-3port.conf
+			cp_func /etc/network/interfaces-3port
+		else
+			rename_func /etc/openblocks/rename/${MODEL}-2port.conf
+			cp_func /etc/network/interfaces-2port
+		fi
 	fi
 	;;
-obshx2)
-	if [ -e /sys/class/net/eno1 ]; then
-		ifrename -c /etc/network/iftab-obshx2-3port
-		cp -a /etc/network/interfaces-3port /etc/network/interfaces
+obshx2*)
+	sleep 5
+
+	rules=/etc/udev/rules.d/75-net-description.rules
+
+	net_ifnames=true
+	grep -q 'net.ifnames=0' /proc/cmdline && net_ifnames=false
+
+	if [ -L $rules -o $net_ifnames = false ] ; then
+		if [ -e /sys/class/net/eth2 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-3port_tmp.conf
+			sleep 0.01
+			rename_func /etc/openblocks/rename/${MODEL}-3port.conf
+			cp_func /etc/network/interfaces-3port
+		else
+			rename_func /etc/openblocks/rename/${MODEL}-2port_tmp.conf
+			sleep 0.01
+			rename_func /etc/openblocks/rename/${MODEL}-2port.conf
+			cp_func /etc/network/interfaces-2port
+		fi
 	else
-		ifrename -c /etc/network/iftab-obshx2-2port
-		cp -a /etc/network/interfaces-2port /etc/network/interfaces
+		if [ -e /sys/class/net/eno1 ]; then
+			rename_func /etc/openblocks/rename/${MODEL}-3port.conf
+			cp_func /etc/network/interfaces-3port
+		else
+			rename_func /etc/openblocks/rename/${MODEL}-2port.conf
+			cp_func /etc/network/interfaces-2port
+		fi
 	fi
 	;;
 obsix9*)
 	first_conf=/etc/openblocks/rename/tmp_rename.conf
 	second_conf=/etc/openblocks/rename/act_rename.conf
 
-	if ( which ifrename 2>&1 ) > /dev/null ; then
-		if [ -f "${first_conf}" -a -f "${second_conf}" ] ; then
-			ifrename -c ${first_conf}
-			sleep 0.01
-			ifrename -c ${second_conf}
-		fi
-	fi
+	func_rename ${first_conf}
+	sleep 0.01
+	func_rename ${second_conf}
 	;;
 esac
 
