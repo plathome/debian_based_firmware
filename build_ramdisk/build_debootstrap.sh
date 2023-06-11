0274c4e0087b3d2ac3ba8760d898f52549d5d25b
Tamotsu Kanoh <kanoh@plathome.co.jp>
2023-06-12 07:55:04 +0900

    removed nfs-kernel-server becouse it not included in debian-12.0.0-arm64-DVD-1.iso

--- build_ramdisk/build_debootstrap.sh
+++ build_ramdisk/build_debootstrap.sh
@@ -148,7 +148,7 @@ buster)
 		;;
 	esac
 	;;
-bullseye|bookworm)
+bullseye)
 	case $TARGET in
 	obsvx1)
 		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,wpasupplicant,ppp,wireless-tools,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,gdisk,dbus"
@@ -168,6 +168,34 @@ bullseye|bookworm)
 		;;
 	obsa16*|obsfx0*|obsfx1*)
 		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,wpasupplicant,ppp,wireless-tools,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,apparmor,gdisk,dbus,nfs-kernel-server"
+		EXCLUDE="nano"
+		;;
+	obsix*|obshx1*|obshx2*)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,gnupg,strace,wget,apparmor,libiw30,gdisk,dbus,systemd-timesyncd"
+		EXCLUDE="nano"
+		;;
+	esac
+	;;
+bookworm)
+	case $TARGET in
+	obsvx1)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,wpasupplicant,ppp,wireless-tools,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,gdisk,dbus"
+		EXCLUDE="nano"
+		;;
+	obsvx2)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,wpasupplicant,ppp,wireless-tools,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,apparmor,gdisk,dbus"
+		EXCLUDE="nano"
+		;;
+	obsbx1)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,wpasupplicant,ppp,wireless-tools,busybox,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,gdisk,dbus"
+		EXCLUDE="nano"
+		;;
+	obsbx1s)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,ethtool,acpi-support-base,wpasupplicant,ppp,wireless-tools,busybox,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,apparmor,gdisk,dbus"
+		EXCLUDE="nano"
+		;;
+	obsa16*|obsfx0*|obsfx1*)
+		INCLUDE="iptables,openssh-server,xz-utils,parted,dosfstools,wpasupplicant,ppp,wireless-tools,bluez,iw,libasound2,libusb-1.0-0,libusb-0.1-4,libnl-route-3-200,initscripts,gnupg,strace,wget,apparmor,gdisk,dbus"
 		EXCLUDE="nano"
 		;;
 	obsix*|obshx1*|obshx2*)
