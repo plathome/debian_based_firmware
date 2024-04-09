22406d1439f3e9fadade0abd7b4efefd0d1f20ce
Tamotsu Kanoh <kanoh@plathome.co.jp>
2024-04-09 10:44:56 +0900

    lacked incliment PATCHLEVEL for obsfx1

--- build_ramdisk/config.sh
+++ build_ramdisk/config.sh
@@ -403,7 +403,7 @@ bullseye)
 		KERNEL=5.4.70
 		ISOFILE=debian-11.1.0-arm64-DVD-1.iso
 		RAMDISK_SIZE=260
-		PATCHLEVEL=1
+		PATCHLEVEL=1dev0
 		DTBFILE=imx8mp-evk-obsfx1-vsc8531.dtb
 #		DTBFILE=imx8mp-evk-obsfx1-rtl8211.dtb
 		DEFCONFIG=obsfx1_defconfig
